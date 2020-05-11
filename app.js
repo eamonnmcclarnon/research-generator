e9e8b823b16d1e132578576bdc91731c6d73e585
eamonnmcclarnon <eamonnmcclarnon@gmail.com>
2020-05-11 17:35:14 +0900

    almost working

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
-var name,monthSalary,savePercent,saveMonths,resultShown,totalSavings;
+var topic1,topic2,topic3,topic4;
 
 
-document.getElementById("save").addEventListener("click", function(){
-    var saveText = document.getElementById("save").value;
-    console.log(saveText);
-    document.getElementById("savelabel").textContent = saveText + "%";
+function Popup(data) {
+    var mywindow = window.open('', 'new div', 'height=600,width=800');
+    mywindow.document.write('<html><head><title></title>');
+    mywindow.document.write('<link rel="stylesheet" href="w3-css.css" type="text/css" media="all">');
+    mywindow.document.write('</head><body >');
+    mywindow.document.write(data);
+    mywindow.document.write('</body></html>');
+    mywindow.document.close();
+    mywindow.focus();
+    setTimeout(function(){mywindow.print();},1000);
 
-});
-
-document.getElementById("months").addEventListener("click", function(){
-    var monthsText = document.getElementById("months").value;
-    console.log(monthsText);
-    document.getElementById("monthslabel").textContent = monthsText + " months";
-
-});
-
-document.getElementById("submitall").addEventListener('click', function(){
-    name = document.getElementById("name").value;
-    monthSalary = document.getElementById("salary").value;
-    savePercent = document.getElementById("save").value;
-    saveMonths = document.getElementById("months").value
-
-    console.log(name);
-    console.log(monthSalary);
-    console.log(savePercent);
-    console.log(saveMonths);
-
-    totalSavings = Math.round((monthSalary * (savePercent/100)) * saveMonths);
-    fotmattedSavings = totalSavings.toLocaleString('ko-KR');
-    document.getElementById("totalsavings").textContent = fotmattedSavings + " won";
-    document.getElementById("resultname").textContent = "Congratulations " + name + "!";
-
-    document.getElementById("resultset").style.display = "block";
-    document.getElementById("dataset").style.display = "none";
-
-
-    
-});
+    return true;
+}
 
 document.getElementById("resetbtn").addEventListener('click', function(){  
     location.reload();
 });
 
-document.getElementById("resultset").style.display = "none";
-document.getElementById("dataset").style.display = "block";
+document.getElementById("submitall").addEventListener("click", function(){
+    topic1 = document.getElementById("topic1").value;
+    document.getElementById("title1").textContent = topic1;
+    topic2 = document.getElementById("topic2").value;
+    document.getElementById("title2").textContent = topic2;
+    topic3 = document.getElementById("topic3").value;
+    document.getElementById("title3").textContent = topic3;
+    topic4 = document.getElementById("topic4").value;
+    document.getElementById("title4").textContent = topic4;
+
+
+
+    document.getElementById("worksheet").style.display = "block";
+    document.getElementById("inputbox").style.display = "none";
+})
+
+document.getElementById("worksheet").style.display = "none";
+document.getElementById("inputbox").style.display = "block";
+
+document.getElementById("printbtn").addEventListener("click", function(){
+    Popup(document.getElementById("worksheetbox").innerHTML);
+})
 
 
 
